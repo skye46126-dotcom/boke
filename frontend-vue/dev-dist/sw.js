8d157616d67ba2459db1b733f20d7f82ea557110
chencc <skye46126@gmail.com>
2026-01-30 22:57:09 +0800

    111

--- frontend-vue/dev-dist/sw.js
+++ frontend-vue/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-6169d802'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.1619thb02lc"
+    "revision": "0.ip28nq3mfm4"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
